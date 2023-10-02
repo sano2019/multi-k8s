ef98ebf202f5543570b121cf5dca381ba985ec10
Sander Nobel <sander.nobel@protonmail.com>
2023-10-02 14:28:03 +0200

    Ready for deployment

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Nodejs 20 minimal flake";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_20
+        ];
+      };
+    };
+}
