d9b99f6b75fb8c0158ece0c7c6cdd4c87bbcded5
Sander Nobel <sander.nobel@protonmail.com>
2023-10-04 17:33:39 +0200

    Updated deploy file

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t sano2019/multi-client:latest -t sano2019/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sano2019/multi-server:latest -t sano2019/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sano2019/multi-worker:latest -t sano2019/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push sano2019/multi-client:latest
+docker push sano2019/multi-server:latest
+docker push sano2019/multi-worker:latest
+
+docker push sano2019/multi-client:$SHA
+docker push sano2019/multi-server:$SHA
+docker push sano2019/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=sano2019/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sano2019/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=sano2019/multi-worker:$SHA
